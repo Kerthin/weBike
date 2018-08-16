f973822b62d95d576424aee1481d9716d795a093
Kerthin <belichenko.roden@list.ru>
2018-08-16 15:49:52 +0300

    Creating a template for project implementation

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+/********************
+	JQuery
+********************/
+//= ../../bower_components/jquery/dist/jquery.min.js
+
+/********************
+	swiper.min.js
+********************/
+//= partials/libs/swiper.min.js
