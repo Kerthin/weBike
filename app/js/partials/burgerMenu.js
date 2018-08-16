1bc722722ba1dee5cd34be5cfd7e54c394727281
Kerthin <belichenko.roden@list.ru>
2018-08-16 23:30:34 +0300

    Create a pop-up menu and add a background for the first page

--- app/js/partials/burgerMenu.js
+++ app/js/partials/burgerMenu.js
@@ -1,3 +1,6 @@
 $('.burgerMenuWrap__burgerMenu').click(function(){
+	menuBlock	=	$('.humburgerPopupMenu');
+
 	$(this).toggleClass('clicked');
+	menuBlock.toggleClass('humburgerPopupMenu_active')
 });
